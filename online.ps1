8c672b05a9865fe86913048536424d64465c42aa
ihaythamadel <123040960+ihaythamadel@users.noreply.github.com>
2024-06-13 19:51:21 +0300

    Update online.ps1

--- online.ps1
+++ online.ps1
@@ -1,9 +1,12 @@
 Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 
+Get-AppXPackage WindowsStore -AllUsers  Foreach {Add-AppxPackage -DisableDevelopmentMode -Register $($_.InstallLocation)AppXManifest.xml}
 
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+$progressPreference = 'silentlyContinue'
+Write-Information Downloading WinGet and its dependencies...
+Invoke-WebRequest -Uri httpsaka.msgetwinget -OutFile $envProgramDataMicrosoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri httpsaka.msMicrosoft.VCLibs.x64.14.00.Desktop.appx -OutFile $envProgramDataMicrosoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri httpsgithub.commicrosoftmicrosoft-ui-xamlreleasesdownloadv2.8.6Microsoft.UI.Xaml.2.8.x64.appx -OutFile $envProgramDataMicrosoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage $envProgramDataMicrosoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage $envProgramDataMicrosoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $envProgramDataMicrosoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
